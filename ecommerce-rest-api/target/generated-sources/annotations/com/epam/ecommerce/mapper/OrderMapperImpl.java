b661ed7ae9f003f735dbb5bbd03be54aba72881f
Yashvi Bhuwalka <yashvi_bhuwalka@epam.com>
2026-05-27 12:52:44 +0530

    Cleaned backend project structure

--- ecommerce-rest-api/target/generated-sources/annotations/com/epam/ecommerce/mapper/OrderMapperImpl.java
+++ ecommerce-rest-api/target/generated-sources/annotations/com/epam/ecommerce/mapper/OrderMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-05-17T23:38:58+0530",
+    date = "2026-05-27T12:49:28+0530",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.9 (Oracle Corporation)"
 )
 @Component
